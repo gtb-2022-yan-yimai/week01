ed86ecf78a46b8a30ac676bf92f4b334a7430d27
yimaiyan <1753695086@qq.com>
2021-11-26 02:38:28 +0800

    add reset&&add and debug list command

--- .todo/do.sh
+++ .todo/do.sh
@@ -3,6 +3,7 @@
 function todo(){
 
 initfile="$PWD/.tasks/init.txt"
+forlist="$PWD/.tasks/forlist.txt"
 
 if [[ ! -n $1 ]] ; then
 
@@ -15,6 +16,7 @@ elif [[ $1 =~ ^list|add|edit|remove|reset|mark|unmark$ ]] && [[ ! -s $initfile ]
 elif [[ $1 =~ ^init$ ]] ; then
 
  touch $initfile
+ touch $forlist
  echo "Initialized successfully."
  echo $1 > $PWD/.tasks/init.txt
 
@@ -29,18 +31,46 @@ echo "Unknown command: '$1'!"
 fi
 
 listfile="$PWD/.tasks/list.txt"
-if [[ $1 =~ ^list$ ]] && [[ ! -s $listfile ]]; then
+if [[ $1 =~ ^list$ ]] && [[ ! -s $listfile ]] && [[ ! -s $forlist ]]; then
  echo " # To be done" >> $PWD/.tasks/list.txt
  echo " Empty" >> $PWD/.tasks/list.txt
  echo " # Completed" >> $PWD/.tasks/list.txt
  echo " Empty" >> $PWD/.tasks/list.txt
- echo "写入成功"
+ echo "写入成功,此行可删去"
 elif [[ $1 =~ ^list$ ]]; then
  awk '{print NR,$0}' $PWD/.tasks/list.txt
 fi
 
 }
 todo $1
+
+
+
+
+if [[ $1 =~ ^add$ ]] && [[  -s $initfile ]] ; then
+ tasks=$@
+ echo $tasks >> $PWD/.tasks/list.txt
+fi
+
+function reset(){
+case "$1" in
+ reset)
+ rm -rf $PWD/.tasks/list.txt
+;;
+esac
+
+ echo " # To be done" >> $PWD/.tasks/list.txt
+ echo " Empty" >> $PWD/.tasks/list.txt
+ echo " # Completed" >> $PWD/.tasks/list.txt
+ echo " Empty" >> $PWD/.tasks/list.txt
+
+}
+reset $1
+
+
+
+
+
 
 
 
