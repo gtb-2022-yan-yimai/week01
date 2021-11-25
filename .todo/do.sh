5789c50b87d2298bc4c92d7867d78c811b815c80
yimaiyan <1753695086@qq.com>
2021-11-26 01:17:16 +0800

    add list & rest

--- .todo/do.sh
+++ .todo/do.sh
@@ -29,12 +29,23 @@ echo "Unknown command: '$1'!"
 fi
 
 listfile="$PWD/.tasks/list.txt"
-if [[ $1 = list ]]; then
-
-
-  echo $listfile
-
+if [[ $1 =~ ^list$ ]] && [[ ! -s $listfile ]]; then
+ echo " # To be done" >> $PWD/.tasks/list.txt
+ echo " Empty" >> $PWD/.tasks/list.txt
+ echo " # Completed" >> $PWD/.tasks/list.txt
+ echo " Empty" >> $PWD/.tasks/list.txt
+ echo "写入成功"
+elif [[ $1 =~ ^list$ ]]; then
+ awk '{print NR,$0}' $PWD/.tasks/list.txt
 fi
 
 }
 todo $1
+
+
+
+
+
+
+
+
--- .todo/todo.sh
+++ .todo/todo.sh
@@ -7,3 +7,38 @@ while $1 = list do
 
 done
 
+function add(){
+    
+    tasks=$@
+    echo $tasks > $PWD/.tasks/list.txt
+}
+add $@
+
+function edit(){
+ echo
+}
+edit $1
+
+function remove(){
+    echo
+}
+remove $1
+
+function mark(){
+    echo
+}
+mark $1
+
+function unmark(){
+    echo
+}
+unmark $1
+
+function reset(){
+case "$1" in
+ reset)
+ echo rm -rf $PWD/.tasks/list.txt
+;;
+esac
+}
+reset $1
